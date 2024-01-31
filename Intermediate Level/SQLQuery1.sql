0325d7285eac11654b9429de3a1f7592d5760166
Sudip Pradhan <104957400+Sudippdn@users.noreply.github.com>
2024-02-01 01:58:40 +0545

    Delete Intermediate Level directory

--- Intermediate Level/SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [EmployeeID]
-      ,[FirstName]
-      ,[LastName]
-      ,[Age]
-      ,[Gender]
-  FROM [SQL tutorial].[dbo].[EmployeeDemoGraphics]
--- Intermediate Level/SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [EmployeeId]
-      ,[JobTitle]
-      ,[Salary]
-  FROM [SQL tutorial].[dbo].[EmployeeSalary]
